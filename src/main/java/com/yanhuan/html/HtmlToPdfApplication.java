42271df560cceca6833f70cbf9a4e48d10ae70b9
yanhuan <yanhuan6252@163.com>
2021-01-14 23:00:26 +0800

    init

---
+++ src/main/java/com/yanhuan/html/HtmlToPdfApplication.java
@@ -0,0 +1,13 @@
+package com.yanhuan.html;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtmlToPdfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HtmlToPdfApplication.class, args);
+	}
+
+}
